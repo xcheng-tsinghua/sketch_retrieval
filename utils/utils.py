a36647736311fd77978b2ada22cfe484039584d2
xcheng-tsinghua <786682191@qq.com>
2026-03-09 21:54:36 +0800

    change

--- utils/utils.py
+++ utils/utils.py
@@ -370,8 +370,8 @@ def get_save_str(args):
 
     save_str = (args.sketch_model + '_' +
                 args.image_model + '_' +
-                retrieval_mode + '_' +
                 args.task +
+                retrieval_mode + '_' +
                 is_full_train +
                 pair_mode +
                 args.add_str)
